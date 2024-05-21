892fb269df2dc9d09a606254a8118959d54bcaeb
pretzelogical <yingyangyo266@gmail.com>
2024-05-21 14:38:01 -0500

    Add base to astro config, Still need to modify internal page links to be prefixed with the base

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://github.com/pretzelogical'
+  site: 'https://github.com/pretzelogical',
+  base: 'pretzelnet'
 });
