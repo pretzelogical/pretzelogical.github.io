7c254ade4338d7eca103da0a0da35c961fc12607
pretzelogical <yingyangyo266@gmail.com>
2024-08-14 12:48:50 -0500

    change site to main github page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://github.com/pretzelogical',
-  base: 'pretzelnet'
+  site: 'https://pretzelogial.github.io',
 });
